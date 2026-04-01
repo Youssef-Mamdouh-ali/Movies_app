8a634adedb9924db394bb5ec9940aa740132e89a
mamin144 <mimoamin70@gmail.com>
2026-04-01 04:24:23 +0200

    after fix the browse error and navigate to details screen

--- lib/Features/home/presentation/widgets/BrowseViewContent.dart
+++ lib/Features/home/presentation/widgets/BrowseViewContent.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bounceable/flutter_bounceable.dart';
 import 'package:movies_app_project/Features/home/presentation/manger/layout_bloc.dart';
 import 'package:movies_app_project/Features/home/presentation/widgets/tab_bar_widget.dart';
 import 'package:movies_app_project/core/utils/theme/app_colors.dart';
 import '../../../../core/widgets/custom_card_widget.dart';
 import '../../../../core/utils/app_assets/app_assets.dart';
+import '../screens/movie_details_view.dart';
 
 class BrowseViewContent extends StatefulWidget {
   const BrowseViewContent({super.key});
@@ -41,17 +43,27 @@ class _BrowseViewContentState extends State<BrowseViewContent> {
                   } else if (state is BrowseLoaded) {
                     final movies = state.movies;
                     return GridView.builder(
-                      gridDelegate:
-                      const SliverGridDelegateWithFixedCrossAxisCount(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 2,
                         childAspectRatio: 0.7,
                       ),
                       itemCount: movies.length,
                       itemBuilder: (context, index) {
                         final movie = movies[index];
-                        return CustomCardWidget(
-                          imagePath: movie.image ,
-                          rate: movie.rating,
+
+                        return Bounceable(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => MovieDetailsView(movieId: movie.id),
+                              ),
+                            );
+                          },
+                          child: CustomCardWidget(
+                            imagePath: movie.image,
+                            rate: movie.rating,
+                          ),
                         );
                       },
                     );
